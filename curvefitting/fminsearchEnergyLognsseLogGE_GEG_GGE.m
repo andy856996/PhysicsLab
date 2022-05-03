985c9d73af963851d03ab6cf017f8a5d40a5819a
DingZeAn <andy856996@gmail.com>
2022-05-03 09:49:26 +0800

    fix curve fitting function

--- curvefitting/fminsearchEnergyLognsseLogGE_GEG_GGE.m
+++ curvefitting/fminsearchEnergyLognsseLogGE_GEG_GGE.m
@@ -75,6 +75,7 @@ X = data(:,1);
 y = data(:,2);
 eval(['model_y =' formula]);
 E = sum(abs(log(y)-log(model_y))./log(y).^2);
+%E = sum((abs(log(y)-log(model_y))./log(y)).^2);
 end
 %% Fmin log
 function E = fun_Casino_in_Fminsearch_Log(x)
